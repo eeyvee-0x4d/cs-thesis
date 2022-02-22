e6b6ba3b8b04ff01da58cd2b39c2b71392a1524f
Al Vincent Musa <alvincentvibar.musa@bicol-u.edu.ph>
2022-02-22 13:01:56 +0800

    added data table TODO: make it responsive

--- client/src/components/Cards/CardTable.vue
+++ client/src/components/Cards/CardTable.vue
@@ -1,10 +1,79 @@
 <template>
-	<div>
-		Hello
+	<div class="relative w-full p-4">
+		<div class="rounded-t mb-0 bg-transparent">
+			<div class="flex flex-wrap items-center">
+				<div class="relative w-full max-w-full flex-grow flex-1">
+					<h2 class=" text-base font-bold">
+						Chart Title
+					</h2>
+				</div>
+			</div>
+		</div>
+		<table class="w-full table-auto text-left">
+			<!-- start table header -->
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Date</th>
+					<th>Tweet</th>
+					<th>Sentiment</th>
+				</tr>				
+			</thead>
+			<!-- end table header -->
+			<!-- start table body -->
+			<tbody>
+				<tr v-for="item, index in data">
+					<td>{{ index+1 }}</td>
+					<td>{{ item.date }}</td>
+					<td>{{ item.tweet }}</td>
+					<td>{{ item.class }}</td>
+				</tr>
+			</tbody>
+			<!-- end table body -->
+			<!-- start table footer -->
+			<tfoot>
+				
+			</tfoot>
+			<!-- end table footer -->
+		</table>
 	</div>
 </template>
 
 <script>
 	import { defineComponent, ref } from "vue";
+
+	export default defineComponent({
+		name: 'CardTable',
+		components: {
+		},
+		data() {
+			const data = [
+				{
+					"date": "01-01-2000",
+					"tweet": "Hello World",
+					"class": "Positive"
+				},
+				{
+					"date": "01-01-2000",
+					"tweet": "Hello World",
+					"class": "Positive"
+				}
+			];
+
+			return { data }
+		}
+	});
 	
 </script>
+
+<style>
+	th {
+		border:  1px solid #ddd;
+		padding: .5rem;
+	}
+
+	td {
+		border:  1px solid #ddd;
+		padding: 0.5rem;
+	}
+</style>
